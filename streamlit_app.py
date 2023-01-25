69e32ee2a99983841585c05b44c9e884388d3191
jgharrison311 <123594653+jgharrison311@users.noreply.github.com>
2023-01-25 17:53:15 -0500

    Update streamlit_app.py
    Example of error from bug in Python code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 ## First application to use Streamlit API for Python (Snowflake)
 import streamlit.py
+streamlit.header('What I had for breakfast')
+streamlit.title('Remember Breakfast?')
+streamlit.text('I think it was Cream of Wheat...')
+streamlit.text('Are you sure?')
+streamit.text('Hmmm... Well, that might have been yesterday.')
