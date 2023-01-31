97c363879dca5554ac2f9df2ed06fd7b57addb57
jgharrison311 <123594653+jgharrison311@users.noreply.github.com>
2023-01-31 12:34:38 -0500

    Update streamlit_app.py
    Added emojis to breakfast line items

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit.py
 
 streamlit.title('Remember Breakfast?')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥤 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥚 Hard-Boiled Free-Range Egg')
